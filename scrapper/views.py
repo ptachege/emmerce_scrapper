d1b21ae8413b12b1dacb99dfbfd4e451c190c97d
muthwii <wambuamuthwii@gmail.com>
2022-08-14 22:15:25 +0300

    Update views.py

--- scrapper/views.py
+++ scrapper/views.py
@@ -106,7 +106,7 @@ def Hotpointentry(request):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--remote-debugging-port=9222')
-        chrome_options.add_argument("--window-size=1920,1200")
+#         chrome_options.add_argument("--window-size=1920,1200")
         driver = webdriver.Chrome(
             '/usr/bin/chromedriver', options=chrome_options)
         
